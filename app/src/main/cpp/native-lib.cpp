46340d4c2f924c0599d357e34cb193e49109fde5
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2022-08-08 16:23:42 +0900

    Add files

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,21 @@
 #include <jni.h>
-#include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_feature_1detection_1android_1ndk_1opencv4android_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+#include <opencv2/opencv.hpp>
+
+static const int NUMBER_OF_FEATURES = 1000;
+static cv::Ptr<cv::Feature2D> DETECTOR = cv::SIFT::create(NUMBER_OF_FEATURES);
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_feature_1detection_1android_1ndk_1opencv4android_MainActivity_drawKeypoints(
+        JNIEnv *env, jobject thiz, jlong mat) {
+    cv::Mat &frame_rgba = *(cv::Mat *) mat;
+
+    cv::Mat frame_gray;
+    cv::cvtColor(frame_rgba, frame_gray, cv::COLOR_RGBA2GRAY);
+
+    std::vector<cv::KeyPoint> keypoints;
+    DETECTOR->detect(frame_gray, keypoints);
+
+    cv::drawKeypoints(frame_rgba, keypoints, frame_rgba);
 }
--- app/src/main/java/com/example/feature_detection_android_ndk_opencv4android/MainActivity.java
+++ app/src/main/java/com/example/feature_detection_android_ndk_opencv4android/MainActivity.java
@@ -1,36 +1,115 @@
 package com.example.feature_detection_android_ndk_opencv4android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.SurfaceView;
+import android.widget.Toast;
 
-import com.example.feature_detection_android_ndk_opencv4android.databinding.ActivityMainBinding;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
-    // Used to load the 'feature_detection_android_ndk_opencv4android' library on application startup.
+public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1;
+
     static {
+        System.loadLibrary("opencv_java4");
         System.loadLibrary("feature_detection_android_ndk_opencv4android");
     }
 
-    private ActivityMainBinding binding;
+    private CameraBridgeViewBase cameraBridgeView;
+    private final BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            if (status == LoaderCallbackInterface.SUCCESS) {
+                cameraBridgeView.enableView();
+            } else {
+                super.onManagerConnected(status);
+            }
+        }
+    };
+
+    public native void drawKeypoints(long mat);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                CAMERA_PERMISSION_REQUEST_CODE);
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        cameraBridgeView = findViewById(R.id.java_camera_view);
+        cameraBridgeView.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeView.setCvCameraViewListener(this);
+        cameraBridgeView.setCameraIndex(0); // front = 1
     }
 
-    /**
-     * A native method that is implemented by the 'feature_detection_android_ndk_opencv4android' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE && grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            cameraBridgeView.setCameraPermissionGranted();
+        } else {
+            Toast.makeText(this, "CAMERA PERMISSION DENIED", Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (cameraBridgeView != null) {
+            cameraBridgeView.disableView();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (OpenCVLoader.initDebug()) {
+            loaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        } else {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, loaderCallback);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (cameraBridgeView != null) {
+            cameraBridgeView.disableView();
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        //
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        //
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat frame = inputFrame.rgba();
+        drawKeypoints(frame.getNativeObjAddr());
+        return frame;
+    }
 }
